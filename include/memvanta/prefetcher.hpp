1d8bd851627d087dc56b372b99c6667d041a2d9b
SAURAV SINGLA <sauravsingla08@gmail.com>
2026-09-18 17:59:32 +0530

    Propagate prefetch worker failures safely

--- include/memvanta/prefetcher.hpp
+++ include/memvanta/prefetcher.hpp
@@ -5,6 +5,7 @@
 #include <condition_variable>
 #include <cstdint>
 #include <deque>
+#include <exception>
 #include <mutex>
 #include <thread>
 #include <unordered_set>
@@ -15,11 +16,13 @@ public:
   ~Prefetcher();
   void request(std::uint32_t id);
   void stop();
+  void rethrow_if_failed();
 private:
   void loop();
   const TensorStore& store_; TensorCache& cache_;
   std::atomic<bool> stop_{false};
   std::thread worker_; std::mutex mu_; std::condition_variable cv_;
   std::deque<std::uint32_t> q_; std::unordered_set<std::uint32_t> pending_;
+  std::exception_ptr error_;
 };
 }
