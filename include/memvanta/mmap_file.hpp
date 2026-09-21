195566fcb8b2746306ae0ad5f203d7d3cc2923a3
SAURAV SINGLA <sauravsingla08@gmail.com>
2026-09-21 19:40:03 +0530

    Avoid serializing mmap advice in prefetch path

--- include/memvanta/mmap_file.hpp
+++ include/memvanta/mmap_file.hpp
@@ -2,7 +2,6 @@
 #include <atomic>
 #include <cstddef>
 #include <cstdint>
-#include <mutex>
 #include <string>
 namespace memvanta {
 struct MMapAdviceStats {
@@ -32,7 +31,6 @@ private:
   int fd_ = -1;
   std::byte* data_ = nullptr;
   std::uint64_t size_ = 0;
-  mutable std::mutex advice_mu_;
   mutable std::atomic<std::uint64_t> advice_attempts_{0};
   mutable std::atomic<std::uint64_t> advice_failures_{0};
 };
